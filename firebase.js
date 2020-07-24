4ecfb49131ca3a4383f3cc14a0acf95a5d4de591
Connor Mulqueen <connor.mulqueen@simplybusiness.com>
2020-07-24 15:15:23 -0400

    make firebase logic better-er

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
-// TODO: [DONE] Change data structure so that it only needs 1 data read to get
-// [DONE] have only one color palette lol
-// [DONE] send firebase updated tile on paint
-// Cache response and only re-draw the diff on listen
-// listen to changes to firestore
-// host on github pages
-
 
 var db = connectFirebase();
 function connectFirebase() {
+  console.log("connecting...")
   var firebaseConfig = {
     apiKey: "AIzaSyCT-nWZttxI11ea-H9a2SW3iNKMP9pOFOE",
     authDomain: "place-9c500.firebaseapp.com",
@@ -18,93 +12,43 @@ function connectFirebase() {
     appId: "1:863156558290:web:ca4701a7143bb8c2fb500b",
     measurementId: "G-5G17632L0D"
   };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
-
-
-  var app = firebase.initializeApp(firebaseConfig);
-  db = firebase.firestore(app);
+  // Get a reference to the database service
+  var db = firebase.database();
   return db;
 }
 
-// function initGrid(width, hefight) {
-//   for (x=70; x<width; x++) {
-//     for (y=0; y<height; y++) {
-//       console.log(x,y)
-//       data = {
-//         location: [x,y],
-//         color: colorPallete[Math.floor(Math.random()*colorPallete.length)]
-//       }
-//       db.collection("tiles").doc(x + ", " + y).set(data)
-//     }
-//   }
-// }
-
-function getAllTiles() {
-  db.collection("place").doc("testing").get().then(function(doc) {
-    if (doc.exists) {
-       data = doc.data()
-       console.log(doc.data())
-        for (var i in data) {
-          x = parseInt(i.substr(0,i.indexOf('_')))
-          y = parseInt(i.substr(i.indexOf('_')+1))
-          addTile(x, y, data[i])
-        }
-        draw()
-    } else {
-        // doc.data() will be undefined in this case
-        console.log("No such document!");
-    }
-  }).catch(function(error) {
-      console.log("Error getting document:", error);
+function writeTile(x,y,color) {
+  db.ref('/tiles/' + 'tile_'+x+'_'+y).set({
+    x: x,
+    y: y,
+    color : color
   });
 }
 
-function updateTileOnDB(x,y,color) {
-  payload = {}
-  payload[x+"_"+y]=color
-  db.collection("place").doc("testing").update(payload)
-}
-
-function removeThing() {
-  db.collection("place").doc("testing").update({indexName: firebase.firestore.FieldValue.delete()})
-}
-
-
-// function testAddTilesToDB(width, height) {
-
-//   var docData = {}
-
-//   for(x=0; x<width; x++) {
-//     for(y=0; y<height; y++) {
-//       docData[x+"_"+y] = colorPallete[Math.floor(Math.random()*colorPallete.length)]
-//     }
-//   }
-//   console.log(docData)
-//   db.collection("place").doc("testing").set(docData).then(function () {
-//       console.log("Document successfully written!");
-//   });
-// };
-
-function listenToDB() {
-  console.log("listening...")
-  let oldData = {}
-  db.collection("testCollection")
-    .on("value"), snap => {
-      console.log(snap.value)
+function initDB() {
+  return db.ref('/tiles/').once('value').then(function(snapshot) {
+    tiles = snapshot.val()
+    // console.log(snapshot.val())
+    for (index in tiles) {
+      tile = tiles[index]
+      addTile(tile.x,tile.y, tile.color)
     }
+  }).then(_ => {
+    console.log('finished loading')
+    document.getElementById("loader").style.display = 'none'
+    document.getElementById("loaded").style.display= 'inline'
+    draw()
+
+    db.ref().child('/tiles/').on('child_changed', function(data) {
+      console.log('child_changed ', data.val())
+      let tile = data.val()
+      addTile(tile.x, tile.y, tile.color)
+      draw()
+    });
+  });
 }
 
-function testListenToDB() {
-  // db.collection("place").doc("testing").on("value", function (dataSnapshot){
-  //   console.log(dataSnapshot)
-  //       console.log(dataSnapshot.data())
-  // })
-  ref = db.collection("place").doc("testing")
-  console.log(ref)
-
-// db.collection("testCollection").doc("test").on("child_changed", function(data) {
-  // console.log(data.val());
-// })
-}
-
-// getAllTiles()
+initDB()
