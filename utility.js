3abbedd55ac2137931feeaaba65e38b5c558e79c
Connor Mulqueen <connor.mulqueen@simplybusiness.com>
2020-07-24 16:54:50 -0400

    remove unused utility file

--- utility.js
+++
@@ -1,7 +0,0 @@
-// function addTiles(width,height) {
-//   for(x=0; x<width; x++) {
-//     for(y=0; y<height; y++) {
-//       writeTile(x,y,colorPallete[Math.floor(Math.random()*colorPallete.length)])
-//     }
-//   }
-// }
